8f18ca06cc7ffeef97eda11ab727cdab9d8fb8db
Sy <christophe.scherly@metas.ch>
2020-11-28 11:03:14 +0100

    déclaration des propriétés déplacée dans le constructeur

--- src/app/shared/models/ingredient.model.ts
+++ src/app/shared/models/ingredient.model.ts
@@ -1,8 +1,6 @@
 export class Ingredient{
-    public name: string;
-    public quantity: number;
 
-    constructor(name: string, quantity: number){
+    constructor(public name: string, public quantity: number){
         this.name = name;
         this.quantity = quantity;
     }
